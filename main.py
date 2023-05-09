3678063683da7fc55c660d1edbadd9d9758aaf91
SiDDyy007 <siddhantp0@gmail.com>
2023-05-09 12:41:56 -0700

    Input Functions

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ def input_puzzle(self):
     # Accept the puzzle input from the user and return it as a matrix
     puzzle = [input().split(" ") for _ in range(3)]
     return puzzle
+
+#INPUT FUNCTIONS
+def choose_heuristic(self):
+    """
+    Prompt the user to choose a heuristic function and set the chosen heuristic.
+    """
+    print("Choose the heuristic function:")
+    print("1. Uniform Cost Search")
+    print("2. A* with the Misplaced Tile heuristic (Manhattan distance)")
+    print("3. A* with the Euclidean distance heuristic")
+    choice = int(input("Enter the number of your choice: "))
+    if choice == 1:
+        self.heuristic = "uniform"
+    elif choice == 2:
+        self.heuristic = "manhattan"
+    elif choice == 3:
+        self.heuristic = "euclidean"
+    else:
+        raise ValueError("Invalid choice")
