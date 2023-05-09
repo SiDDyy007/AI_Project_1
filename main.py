48fb0cbb9fa478bc5e9c259314d99fa74e7b9d19
SiDDyy007 <siddhantp0@gmail.com>
2023-05-09 12:38:04 -0700

    Input Functions

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 import math
 import heapq
 print ("Hello World")
+
+#INPUT FUNCTIONS
+def input_puzzle(self):
+    """
+    Accepts the puzzle input from the user and return it as a matrix.
+
+    :Returns -> The input puzzle matrix.
+    """
+    # Accept the puzzle input from the user and return it as a matrix
+    puzzle = [input().split(" ") for _ in range(3)]
+    return puzzle
