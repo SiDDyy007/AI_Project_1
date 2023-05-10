cbb82e1137097502b013451e38b2966df1475bbe
SiDDyy007 <siddhantp0@gmail.com>
2023-05-10 14:24:09 -0700

    Puzzle Node work

--- puzzle_node.py
+++ puzzle_node.py
@@ -4,3 +4,12 @@ class PuzzleNode():
         self.depth = depth
         self.h_val = h_val
         self.g_val = 0
+    
+    def __lt__(self, other):
+        return self.g_val < other.g_val
+
+    def find_space(self):
+        for row in range(len(self.data)):
+            for col in range(len(self.data)):
+                if self.data[row][col] == 0:
+                    return row, col
