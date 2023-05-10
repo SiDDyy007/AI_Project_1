071a3ffe094d742d3faf8c807b0a94a15dde7ba4
SiDDyy007 <siddhantp0@gmail.com>
2023-05-10 14:26:49 -0700

    Puzzle Node work

--- puzzle_node.py
+++ puzzle_node.py
@@ -13,3 +13,23 @@ class PuzzleNode():
             for col in range(len(self.data)):
                 if self.data[row][col] == 0:
                     return row, col
+    def generate_children(self):
+        n = len(self.data)
+        blank_row, blank_col = self.find_space(0)
+        possible_moves = [
+            (blank_row, blank_col - 1),
+            (blank_row, blank_col + 1),
+            (blank_row - 1, blank_col),
+            (blank_row + 1, blank_col)
+        ]
+        children = []
+        
+        for row, col in possible_moves:
+            if 0 <= row < n and 0 <= col < n:
+                # Create a new matrix with the blank space moved to the new position
+                new_matrix = [row.copy() for row in self.data]
+                new_matrix[blank_row][blank_col], new_matrix[row][col] = new_matrix[row][col], new_matrix[blank_row][blank_col]
+                child = PuzzleNode(new_matrix, self.level + 1, 0)
+                children.append(child)
+                
+        return children
