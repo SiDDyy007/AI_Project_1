30819f9c3ab58183d789893c4e69ae6789a0e4b4
SiDDyy007 <siddhantp0@gmail.com>
2023-05-14 11:30:43 -0700

    Tree Class

--- tree_class.py
+++ tree_class.py
@@ -1,11 +1,15 @@
 #Solution Tree Classes
 
-
+'''Initialize a Node with given data and parent.'''
 class Node:
     def __init__(self, data, parent=None):
+        '''The data to be stored in the node.'''
         self.data = data
+        '''The parent node of this node, default is None.'''
         self.parent = parent
-
+        
+'''Initialize a Tree with the given root data.'''
 class Tree:
     def __init__(self, root_data):
+        '''The data to be stored in the root node.'''
         self.root = Node(root_data)
